b9040c5a2c515fb43dd3a689c839345a1bebe52c
diogoalfa <Diogo@MacBook-Pro-de-Diego.local>
2013-12-16 12:25:54 -0300

    Tarea N 04 ayudantia CRUD

---
+++ CodeIgniter_2.1.4/application/views/general/menu_principal.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+
+  
+?>    
+    <body>
+ <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    
+        
+        <div class="masthead" id="divBarra1">
+        <h3 class="muted">Horario de Clases</h3>
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <ul class="nav">
+                <li class="active"><a href="<?= site_url('welcome');?>">Inicio</a></li>
+                <li><a href="#">Consulta</a></li>
+                <li><a href="<?= site_url('pedidos');?>">Pedidos</a></li>
+                    <li><a href="#">Profesores</a></li>
+                <li><a href="#">Ayuda</a></li>
+                <li><a href="<?= site_url('intranet');?>">Intranet</a></li>
+                <li><a href="<?= site_url('contacto');?>">Contacto</a></li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- /.navbar -->
+      </div>
---
+++ CodeIgniter_2.1.4/application/views/pedidos/pedir_sala.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+<div class="well">
+    <h4>Bienvenidor Profesores!</h4><br>
+    LLene el formulario para solicitar una sala disponible <br><br>
+    <div class="row">
+        <div class="span2"><label>Rut :</label></div><div class="span4"><input type="text" placeholder=""></div>
+    </div>
+    <div class="row">
+       <div class="span2"><label>Nombre :</label></div><div class="span4"><input type="text" placeholder=""></div>
+    </div>
+    <div class="row">
+        <div class="span2"><label>Apellidos :</label></div><div class="span4"><input type="text" placeholder=""></div>
+    </div>
+    <div class="row">
+        <div class="span2"><label>Asignatura : </label></div><div class="span4"><input type="text" placeholder=""></div>
+    </div>
+     <div class="row">
+        <div class="span2"><label>Contacto : </label></div><div class="span4"><input type="text" placeholder="example@gmail.com"></div>
+    </div> <div class="row">
+        <div class="span2"><label>Dia de la semana : </label></div>
+        <div class="span4">
+            <select>
+                <option>Lunes</option>
+                <option>Martes</option>
+                <option>Miercoles</option>
+                <option>Jueves</option>
+                <option>Viernes</option>
+            </select>
+        </div>
+    </div>
+     <div class="row">
+        <div class="span2"><label>Periodo : </label></div>
+       
+             <div class="span4">
+                 <select>
+                <option>I</option>
+                <option>II</option>
+                <option>III</option>
+                <option>IV</option>
+                <option>V</option>
+                <option>VI</option>
+                <option>VII</option>
+                <option>VIII</option>
+            </select>
+        </div>
+    </div>
+    <div class="row">
+        <div class="span2"><label>Sala a priori : </label></div><div class="span4"><input type="text" placeholder=""></div>
+    </div>
+    <div class="row">
+        <div class="span2">Motivo : </label></div><div class="span4"><textarea rows="3"></textarea></div>
+    </div>
+    <div class="row">
+        <div class="span3"><button type="submit" class="btn">Enviar</button> </div>
+    </div>
+    
+</div>
